b86b383bcd37964d3820e509a6097a9934f6f438
bernardoenock <bernardoenockgm@gmail.com>
2023-12-23 14:03:00 -0300

    fix: Generate new migration

--- backend/prisma/migrations/20231223155948_init/migration.sql
+++ backend/prisma/migrations/20231223155948_init/migration.sql
@@ -11,34 +11,20 @@ CREATE TABLE "Users" (
 -- CreateTable
 CREATE TABLE "DeliveryOrder" (
     "id" UUID NOT NULL,
-    "status" TEXT NOT NULL,
-    "userID" UUID,
-
-    CONSTRAINT "DeliveryOrder_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "DeliveryAddress" (
-    "id" UUID NOT NULL,
     "country" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "state" TEXT NOT NULL,
     "street" TEXT NOT NULL,
     "district" TEXT NOT NULL,
     "code" TEXT NOT NULL,
-    "orderID" UUID NOT NULL,
+    "status" TEXT NOT NULL,
+    "userID" UUID,
 
-    CONSTRAINT "DeliveryAddress_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "DeliveryOrder_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
--- CreateIndex
-CREATE UNIQUE INDEX "DeliveryAddress_orderID_key" ON "DeliveryAddress"("orderID");
-
 -- AddForeignKey
 ALTER TABLE "DeliveryOrder" ADD CONSTRAINT "DeliveryOrder_userID_fkey" FOREIGN KEY ("userID") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "DeliveryAddress" ADD CONSTRAINT "DeliveryAddress_orderID_fkey" FOREIGN KEY ("orderID") REFERENCES "DeliveryOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
