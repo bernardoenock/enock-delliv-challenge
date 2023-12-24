6d5e700fd7d492ff714d89dbcd767b573cc0ff51
bernardoenock <bernardoenockgm@gmail.com>
2023-12-24 03:34:24 -0300

    feat: Clean App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
-import RegistrationForm from './components/RegisterForm';
-import Home from './components/Home';
+import { CssBaseline } from "@mui/material"
+import RoutersApp from "./components/templates/RoutersApp"
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <CssBaseline />
+      <RoutersApp />
+    </>
+  )
+}
 
-        <Routes>
-          <Route
-            path="/"
-            element={<LoginForm />}
-          />
-          <Route
-            path="/register"
-            element={<RegistrationForm />}
-          />
-          <Route
-            path="/home"
-            element={<Home />}
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default App
