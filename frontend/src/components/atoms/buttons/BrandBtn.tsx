b7025a2bba53444584cf2fa1e9da70dda4dfa956
bernardoenock <bernardoenockgm@gmail.com>
2023-12-25 17:51:33 -0300

    feat: Create BrandBtn component

---
+++ frontend/src/components/atoms/buttons/BrandBtn.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { IconButton, Badge } from "@mui/material"
+import Logo from "../Logo"
+import { useSelector } from "react-redux"
+import { RootState } from "../../../app/store"
+
+function BrandBtn() {
+  const deliveryOrders = useSelector(
+    (state: RootState) => state.delivery.deliveryOrders,
+  )
+
+  return (
+    <IconButton sx={{ backgroundColor: "white" }}>
+      <Badge badgeContent={deliveryOrders.length} color="primary">
+        <Logo />
+      </Badge>
+    </IconButton>
+  )
+}
+
+export default BrandBtn
