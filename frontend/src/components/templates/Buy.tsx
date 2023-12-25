f76b095429269006ca85f4c5157247abe9233fa7
bernardoenock <bernardoenockgm@gmail.com>
2023-12-25 17:58:23 -0300

    feat: Create Buy component

---
+++ frontend/src/components/templates/Buy.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import RegistrationForm from "../molecules/RegisterForm"
+import BodyPage from "../organisms/BodyPage"
+
+function Buy() {
+  return (
+    <BodyPage>
+      <RegistrationForm />
+    </BodyPage>
+  )
+}
+
+export default Buy
