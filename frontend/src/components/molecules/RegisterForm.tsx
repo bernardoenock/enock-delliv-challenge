77fa8c487efc135de73f4861ac836f6ab90676c8
bernardoenock <bernardoenockgm@gmail.com>
2023-12-27 19:05:44 -0300

    feat: Apply ToastAlert in RegistrationForm

--- frontend/src/components/molecules/RegisterForm.tsx
+++ frontend/src/components/molecules/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { register } from "../../features/auth/authActions"
 import { useNavigate } from "react-router-dom"
@@ -8,8 +8,11 @@ import InputName from "../atoms/inputs/InputName"
 import InputEmail from "../atoms/inputs/InputEmail"
 import InputPassword from "../atoms/inputs/InputPassword"
 import SingInBtn from "../atoms/buttons/SingInBtn"
+import { Typography } from "@mui/material"
+import { AuthActionTypes } from "src/features/auth/auth.interface"
+import ToastAlert from "../atoms/ToastAlert"
 
-function RegistrationForm(){
+function RegistrationForm() {
   const dispatch = useDispatch()
   const error = useSelector((state: any) => state.error)
 
@@ -26,23 +29,50 @@ function RegistrationForm(){
   }
 
   const navigate = useNavigate()
-  const token = useSelector((state: RootState) => state.auth.token)
+  const { auth } = useSelector((state: RootState) => state)
+
+  console.log("Type Register", auth.type)
+  console.log("ERROR Register", auth.error)
+
+  const [toastAlertState, setToastAlertState] = useState<boolean>(false)
+  const [toastMessage, setToastMessage] = useState<string>('')
+
+  const handleToast = () => {
+    if (auth.type === AuthActionTypes.REGISTER_FAILURE) {
+      setToastMessage("ERRO: Usuario pode já estar cadastrado.")
+      setToastAlertState(true)
+    }
+
+    if (auth.type === AuthActionTypes.REGISTER_SUCCESS) {
+      setToastMessage("Sucesso: Usuario cadastrado!")
+      setToastAlertState(true)
+    }
+  }
 
   useEffect(() => {
-    if (token) {
+    if (auth.token) {
       navigate("/")
     }
-  }, [token, navigate])
+  }, [auth.token, navigate])
 
   return (
-    <FormContainer onSubmit={handleSubmit}>
-      <h2>Crie sua conta</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <InputName />
-      <InputEmail />
-      <InputPassword />
-      <SingInBtn />
-    </FormContainer>
+    <>
+      <FormContainer onSubmit={handleSubmit}>
+        <Typography variant="h5">
+          Crie sua conta para gerenciar seus pedidos!
+        </Typography>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <InputName />
+        <InputEmail />
+        <InputPassword />
+        <SingInBtn />
+      </FormContainer>
+      <ToastAlert
+        isOpen={toastAlertState}
+        handleToast={handleToast}
+        message={toastMessage}
+      />
+    </>
   )
 }
 
