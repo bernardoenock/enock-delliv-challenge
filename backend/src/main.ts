287133b70973b953a079cf16595260fc49ec5f83
bernardoenock <bernardoenockgm@gmail.com>
2023-12-27 19:01:43 -0300

    fix: Add new url for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const cors ={
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:8080'],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'
   }
 
