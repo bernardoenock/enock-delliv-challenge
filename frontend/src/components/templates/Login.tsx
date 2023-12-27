d18170d430fcad05e83c3b37b6e4b26f50a192b9
bernardoenock <bernardoenockgm@gmail.com>
2023-12-27 19:06:09 -0300

    fix: SOLID

--- frontend/src/components/templates/Login.tsx
+++ frontend/src/components/templates/Login.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import BuyBtn from "../atoms/buttons/BuyBtn"
 import LoginForm from "../molecules/LoginForm"
 import BodyPage from "../organisms/BodyPage"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { RootState } from "src/app/store"
 
 function Login() {
+  const navigate = useNavigate()
+  const token = useSelector((state: RootState) => state.auth.token)
+
+  useEffect(() => {
+    if (token) {
+      navigate("/dashboard")
+    }
+  }, [token, navigate])
   return (
     <BodyPage>
       <LoginForm />
