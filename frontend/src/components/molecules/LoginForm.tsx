3960789a420a08f492f26c494f51a85b04e0bad5
bernardoenock <bernardoenockgm@gmail.com>
2023-12-28 02:29:27 -0300

    feat: Add ToastAlert

--- frontend/src/components/molecules/LoginForm.tsx
+++ frontend/src/components/molecules/LoginForm.tsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { login } from "../../features/auth/authActions"
 import SingInBtn from "../atoms/buttons/SingInBtn"
 import InputPassword from "../atoms/inputs/InputPassword"
 import InputEmail from "../atoms/inputs/InputEmail"
 import FormContainer from "../atoms/wrappers/FormContainer"
+import ToastAlert from "../atoms/ToastAlert"
 
-const LoginForm: React.FC = () => {
+function LoginForm() {
+  const [toastAlertState, setToastAlertState] = useState<boolean>(false)
+  const [toastMessage, setToastMessage] = useState<string>("")
+
   const dispatch = useDispatch()
   const error = useSelector((state: any) => state.error)
   console.log("LOGIN ERROR:", error)
@@ -17,15 +21,19 @@ const LoginForm: React.FC = () => {
     dispatch(
       login(data.get("email") as string, data.get("password") as string) as any,
     )
+    setToastMessage("Login sucesso!")
   }
 
   return (
+    <>
     <FormContainer onSubmit={handleSubmit}>
       <h2>Login</h2>
       <InputEmail />
       <InputPassword />
       <SingInBtn />
     </FormContainer>
+    <ToastAlert toastAlertState={toastAlertState} setToastAlertState={setToastAlertState} toastMessage={toastMessage}/>
+    </>
   )
 }
 
